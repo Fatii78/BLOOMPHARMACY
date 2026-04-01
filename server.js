56bf9a61491280b597d5720075fcf0a4b73dce3f
FATEMA ALNAJJAS <fatii78jj@gmail.com>
2026-04-01 14:27:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use("/products", productRouter)
 app.use("/cart", cartRouter)
 app.use("/reviews", reviewRouter)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log("Server running on port 3000")
+  console.log(`Server is running on port ${PORT}`)
 })
